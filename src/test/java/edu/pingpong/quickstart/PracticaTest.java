19fbe1751394eca0b6141ebdd19445af5604d166
BertaVR <bverges@cifpfbmoll.eu>
2021-06-13 16:10:39 +0200

    first commit

---
+++ src/test/java/edu/pingpong/quickstart/PracticaTest.java
@@ -0,0 +1,21 @@
+package edu.pingpong.quickstart;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class PracticaTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
